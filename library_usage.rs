cf24c069a73befc761b85d58498a035529f94b84
peti2001-test <58191549+peti2001-test@users.noreply.github.com>
2022-06-28 15:22:51 +0200

    Update library_usage.rs

--- library_usage.rs
+++ library_usage.rs
@@ -6,3 +6,12 @@ let dec = BigDecimal::from_str(&input).unwrap();
 let float = f32::from_str(&input).unwrap();
 
 println!("Input ({}) with 10 decimals: {} vs {})", input, dec, float);
+
+use bigdecimal::BigDecimal;
+use std::str::FromStr;
+
+let input = "0.8";
+let dec = BigDecimal::from_str(&input).unwrap();
+let float = f32::from_str(&input).unwrap();
+
+println!("Input ({}) with 10 decimals: {} vs {})", input, dec, float);
